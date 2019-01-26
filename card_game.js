5399907f0b78bb1c3a4929f1c812ef76b93a69e0
Jason Karlin <jkarlin929@gmail.com>
2019-01-25 19:17:39 -0500

    Update card_game.js

--- card_game.js
+++ card_game.js
@@ -18,7 +18,7 @@
 
   function buildDeck() {
     const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
-    const deck = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   }
 
